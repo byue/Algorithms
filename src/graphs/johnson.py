5aac9b783b823a22b2c8ee16de2c2bca8361e630
Bryan Yue <yue_bryan123@hotmail.com>
2024-08-20 02:31:10 -0700

    Johnson's Algorithm for All Pairs Shortest Paths

---
+++ src/graphs/johnson.py
@@ -0,0 +1,20 @@
+from src.graphs.bellman_ford import get_weighted_shortest_paths as bellman_ford
+from src.graphs.djikstra import get_weighted_shortest_paths as djikstra
+
+def get_all_pairs_shortest_paths(graph):
+    virtual_graph = graph.copy()
+    for node in graph:
+        virtual_graph.add_edge('Virtual', node, weight=0)
+    distances, _ = bellman_ford('Virtual', virtual_graph)
+    if not distances:
+        return None
+    transformed_graph = graph.copy()
+    for parent, child, data in transformed_graph.edges(data=True):
+        data['weight'] += distances[parent]
+        data['weight'] -= distances[child]
+    djikstra_result = {node: djikstra(node, transformed_graph) for node in transformed_graph}
+    for source in djikstra_result:
+        for destination in djikstra_result[source][0]:
+            djikstra_result[source][0][destination] -= distances[source]
+            djikstra_result[source][0][destination] += distances[destination]
+    return djikstra_result
---
+++ tst/graphs/test_johnson.py
@@ -0,0 +1,39 @@
+from src.graphs.johnson import get_all_pairs_shortest_paths
+import networkx as nx
+
+class TestDjikstra:
+    def test_all_pairs_shortest_paths_negative_cycle(self):
+        graph = nx.DiGraph()
+        graph.add_edge("A", "B", weight=-5)
+        graph.add_edge("B", "A", weight=-5)
+
+        actual_result = get_all_pairs_shortest_paths(graph)
+
+        assert actual_result is None
+
+    def test_all_pairs_shortest_paths_acyclic(self):
+        graph = nx.DiGraph()
+        graph.add_edge("A", "B", weight=-2)
+        graph.add_edge("C", "A", weight=4)
+        graph.add_edge("B", "C", weight=-1)
+        graph.add_edge("C", "X", weight=2)
+        graph.add_edge("C", "Y", weight=-3)
+        graph.add_edge("Z", "X", weight=1)
+        graph.add_edge("Z", "Y", weight=-4)
+
+        expected_result = {'A': ({'A': 0, 'B': -2, 'C': -3, 'X': -1, 'Y': -6, 'Z': float('inf')},
+                                 {'A': None, 'B': 'A', 'C': 'B', 'X': 'C', 'Y': 'C'}),
+                           'B': ({'A': 3, 'B': 0, 'C': -1, 'X': 1, 'Y': -4, 'Z': float('inf')},
+                                 {'B': None, 'C': 'B', 'A': 'C', 'X': 'C', 'Y': 'C'}),
+                           'C': ({'A': 4, 'B': 2, 'C': 0, 'X': 2, 'Y': -3, 'Z': float('inf')},
+                                 {'C': None, 'A': 'C', 'X': 'C', 'Y': 'C', 'B': 'A'}),
+                           'X': ({'A': float('inf'),'B': float('inf'), 'C': float('inf'), 'X': 0, 'Y': float('inf'), 'Z': float('inf')},
+                                 {'X': None}),
+                           'Y': ({'A': float('inf'), 'B': float('inf'), 'C': float('inf'), 'X': float('inf'), 'Y': 0, 'Z': float('inf')},
+                                 {'Y': None}),
+                           'Z': ({'A': float('inf'), 'B': float('inf'), 'C': float('inf'), 'X': 1, 'Y': -4, 'Z': 0},
+                                 {'Z': None, 'X': 'Z', 'Y': 'Z'})}
+
+        actual_result = get_all_pairs_shortest_paths(graph)
+
+        assert actual_result == expected_result
