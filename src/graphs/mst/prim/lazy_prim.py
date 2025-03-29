f6766ae73f9333210260e0dfa3998058f2185f0a
Bryan Yue <yue_bryan123@hotmail.com>
2025-03-29 13:32:52 -0700

    Simplify Lazy Prim

--- src/graphs/mst/prim/lazy_prim.py
+++ src/graphs/mst/prim/lazy_prim.py
@@ -1,28 +1,21 @@
 import networkx as nx
 import heapq
 
-def _visit(graph, node, included_in_mst, pq):
-    included_in_mst.add(node)
-    for edge in graph.edges(node, data=True):
-        u, v, data = edge
-        other_node = v if u == node else u
-        if other_node not in included_in_mst:
-            heapq.heappush(pq, (data['weight'], edge))
-
 def get_mst(graph):
-    pq = []
     starting_node = next(iter(graph))
     mst = nx.Graph()
-    included_in_mst = set()
-    _visit(graph, starting_node, included_in_mst, pq)
+    visited = set([starting_node])
+    pq = [(data['weight'], u, v) for u, v, data in graph.edges(starting_node, data=True)]
+    heapq.heapify(pq)
     total_cost = 0
-    while len(pq) > 0:
-        _, edge = heapq.heappop(pq)
-        u, v, data = edge
-        if u in included_in_mst and v in included_in_mst:
-            continue
-        mst.add_edge(u, v, weight=data['weight'])
-        total_cost += data['weight']
-        for node in filter(lambda node: node not in included_in_mst, [u, v]):
-            _visit(graph, node, included_in_mst, pq)
+    while pq:
+        weight, prev_node, current_node = heapq.heappop(pq)
+        if current_node not in visited:
+            visited.add(current_node)
+            mst.add_edge(prev_node, current_node, weight=weight)
+            total_cost += weight
+            for u, v, data in graph.edges(current_node, data=True):
+                next_node = v if u == current_node else u
+                if next_node not in visited:
+                    heapq.heappush(pq, (data['weight'], current_node, next_node))
     return total_cost, mst
